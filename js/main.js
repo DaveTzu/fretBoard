939d96434b58b5f622b90705eec42664425dd6db
Dave <DisembodiedMinstrel@gmail.com>
2022-05-06 13:05:01 -0600

    added sounds and buttons on 6th string

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+const sounds = document.querySelectorAll('audio')
+butts = document.querySelectorAll('.note')
 
+butts.forEach(but => {
+    but.addEventListener('click', playNote)
+});
+
+function playNote(e) {
+    sounds[e.target.classList[2].split('fret').pop()].play()
+}
